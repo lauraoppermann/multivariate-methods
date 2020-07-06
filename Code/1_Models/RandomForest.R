dd413ff00f1c29c3401d20f49e8495d798e69ddd
felixGer <felixgermaine1@gmail.com>
2020-07-07 00:41:05 +0200

    Update RandomForest.R

--- Code/1_Models/RandomForest.R
+++ Code/1_Models/RandomForest.R
@@ -4,6 +4,13 @@ library("mlr")
 library("penalized")
 library("LiblineaR")
 
+df = createDummyFeatures(
+  df,
+  target = "loan_default",
+  method = "1-of-n",
+  cols = NULL
+)
+
 classif.task = makeClassifTask(id = "CreditScoring", 
                                data = df, 
                                target = "loan_default")
@@ -19,5 +26,5 @@ classif.lrn.imp = makeImputeWrapper(classif.lrn,
                                     dummy.type = "numeric"
  )
 
-rdesc = makeResampleDesc("CV", iters = 3, stratify=TRUE)
+rdesc = makeResampleDesc("CV", iters = 5, stratify=TRUE)
 resample(classif.lrn.imp, classif.task , rdesc, measures = list(auc),models = TRUE)
