289496ad9cac468571666cb17bfe07ed2e237984
felixGer <felixgermaine1@gmail.com>
2020-07-06 20:01:14 +0200

    RandomForest
    Started with implementing Random FOrest

---
+++ Code/1_Models/RandomForest.R
@@ -0,0 +1,23 @@
+setwd("C:/Users/felix/Documents/GitHub/multivariate-methods")
+load("./Code/Data_prep/preproc_data.Rdata")
+library("mlr")
+library("penalized")
+library("LiblineaR")
+
+classif.task = makeClassifTask(id = "CreditScoring", 
+                               data = df, 
+                               target = "loan_default")
+
+classif.lrn = makeLearner("classif.LiblineaRL1LogReg", 
+                          predict.type = "prob", 
+                          fix.factors.prediction = TRUE)
+
+classif.lrn.imp = makeImputeWrapper(classif.lrn, 
+                                    list(numeric = imputeMedian(),
+                                         integer = imputeMedian(), 
+                                         factor =imputeMode()),
+                                    dummy.type = "numeric"
+ )
+
+rdesc = makeResampleDesc("CV", iters = 3, stratify=TRUE)
+resample(classif.lrn.imp, classif.task , rdesc, measures = list(auc),models = TRUE)
--- Code/Data_prep/0_Data_clean.R
+++ Code/Data_prep/0_Data_clean.R
@@ -1,10 +1,14 @@
-install.packages("tidyverse")
-library("lubridate")
-library("stringr")
-
 setwd("C:/Users/felix/Documents/GitHub/multivariate-methods")
 
-data = read.csv("./Data/data.csv")
+#install.packages("tidyverse")
+#install.packages("magrittr")
+
+library("lubridate")
+library("stringr")
+library("dplyr")
+library("magrittr")
+
+df = read.csv("./Data/data.csv")
 
 
 ### Change dates and times to numerics
@@ -13,7 +17,7 @@ data = read.csv("./Data/data.csv")
 date_var_names = list("Date.of.Birth","DisbursalDate")
 
 #function to change cut-off year. Lubridate was changing 68 to 2068 not 1968
-cut_off_year = function(x, year=1910){
+cut_off_year = function(x, year=1921){
   m <- year(x) %% 100
   year(x) <- ifelse(m > year %% 100, 1900+m, 2000+m)
   x
@@ -21,9 +25,9 @@ cut_off_year = function(x, year=1910){
 
 #change dates to days
 for (name in date_var_names){
-  data[, name] = as.numeric(
+  df[, name] = as.numeric(
                   cut_off_year(
-                    lubridate::dmy(data[,name])
+                    lubridate::dmy(df[,name])
                     )
 )
 }
@@ -47,7 +51,30 @@ get_year_months = function(vec){
   return(Years + (Months/12))
 }
 
-data[,date_time_names] = sapply(data[,date_time_names], get_year_months)
+df[,date_time_names] = sapply(df[,date_time_names], get_year_months)
+
+### change types to factor 
+to_factor = c("branch_id", 
+              "supplier_id",
+              "manufacturer_id",
+              "Current_pincode_ID",
+              "Employment.Type",
+              "State_ID", 
+              "PERFORM_CNS.SCORE.DESCRIPTION",
+              "Employee_code_ID")
+
+df %<>% 
+  mutate_at(to_factor, factor)
+
+#drop unused (for now) columns (too many different ID's --> Need to find a solution)
+
+df %<>%
+  select(-c(UniqueID,supplier_id, branch_id, 
+            Current_pincode_ID, Employee_code_ID)
+)
+
+###save file
+save(df, file="./Code/Data_prep/preproc_data.Rdata")
 
 
 
